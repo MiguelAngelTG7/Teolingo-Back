96904456e9586cd9a51d3bcb52ad038889ec477f
MiguelAngelTG7 <michael_angello@hotmail.com>
2025-08-29 00:29:55 -0500

    settings py modified

--- biblielingo_back/settings.py
+++ biblielingo_back/settings.py
@@ -47,6 +47,27 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 # Application definition
 
 INSTALLED_APPS = [
